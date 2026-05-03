4b2c1ca1a66cad530019ed99ac82f00ad1ad6e29
Ubuntu <jaedon-os@ubuntu-os-vm.tuqmrrajrqouxedesfould0cpf.ex.internal.cloudapp.net>
2026-05-03 22:08:32 +0000

    String-pthread.c update

--- String_pthread.c
+++ String_pthread.c
@@ -72,7 +72,30 @@ int readf(FILE *fp)
 void *sub_string(void *threadid) 	/*each process searches in the string with the step of nprocs until it reach or beyond*/ 
 	/*the (n1-n2)th char which is the last possible beginning of the substring*/
 {
+	long tid = (long)threadid;
+	int i, j, k, count;
+	int local_count = 0;
 
+	int start = tid * nlocal;
+	int end = start + nlocal;
+
+	for(i = start; i<end; i++){
+		count = 0;
+		for(j = i, k = 0; k<n2; j++, k++){
+			if(*(s1+j) != *(s2+k))
+				break;
+			else
+				count++;
+			if(count == n2)
+				local_count++;
+		}
+	}
+
+	pthread_mutex_lock(&total_lock);
+	total += local_count;
+	pthread_mutex_unlock(&total_lock);
+
+	pthread_exit(NULL);
 }
 
 
